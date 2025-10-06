4d9f50055a66dd762faec35e9215fb60bce696ce
usamaa1 <usamariz1997@gmail.com>
2025-10-06 20:32:20 +0500

    Add project files.

---
+++ 2404G1/Program.cs
@@ -0,0 +1,138 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//var c = 'a';
+//var b = false;
+
+
+
+
+
+//'a'
+//"klfdsgklfdk"
+
+//int number;
+
+//number = "h";
+//number = 'h';
+//Console.WriteLine(number);
+
+// Datatypes 
+
+//string str = 'j';
+//string str = "fdjksjfkjsdkjfsdkj";
+//bool b = false;
+//char c = 'a';
+//float f = 2.3f;
+//decimal d = 2.3m;
+//double db = 2.4;
+//long l = 2343242342343;
+//var v = 435;
+
+//string? z = "jfkdsjf";
+//Type type;
+
+
+//Console.WriteLine(f);
+//Console.WriteLine(db);
+
+
+
+
+
+
+
+//functions
+
+//int fun()
+//{
+//    return 23;
+//}
+
+//string fun2()
+//{
+//    return "jfksdjfkjsd";
+//}
+
+//bool fun3()
+//{
+//    return true;
+//}
+
+
+//int add(int x, int y) 
+//{ 
+
+//    return x + y;
+
+//}
+
+//Console.WriteLine(add(3, 4));
+//int x=5; int y=76;
+//void add()
+//{
+
+//   Console.WriteLine(x + y);
+
+//}
+
+//add();
+
+
+
+
+
+//var name = "Arif";
+//var age = 23;
+
+
+//Console.WriteLine("My name is: " + name + " and my age is: " + age);
+
+//Console.WriteLine("My name is: {0} and my age is: {1}", name, age);
+//Console.WriteLine($"My name is: {name} and my age is: {age}");
+//Console.WriteLine($"My name is: {name}\n\r and my age is:\n {age}");
+
+
+//int num1 = 23;
+//int num2 = 5;
+
+//Console.WriteLine(num1 + num2);
+//Console.WriteLine(num1 - num2);
+//Console.WriteLine(num1 * num2);
+//Console.WriteLine(Convert.ToDecimal(num1) / Convert.ToDecimal(num2));
+//Console.WriteLine(Convert.ToDecimal(num1) % Convert.ToDecimal(num2));
+
+
+
+//Console.WriteLine("Enter your Name ");
+//var userInput = Console.ReadLine();
+
+//Console.WriteLine($"My name Is: {userInput}");
+
+
+//Console.WriteLine("Enter your age: ");
+
+//string userAge = Console.ReadLine();
+
+//if(Convert.ToInt64(userAge) >= 18)
+//    Console.WriteLine("You are eligible");
+//else
+//    Console.WriteLine("You are not eligible");
+
+
+
+//Modulus with if else statement
+
+//int num = Convert.ToInt32(Console.ReadLine());
+
+
+//if (num % 2 == 1)
+//    Console.WriteLine("Odd Number");
+//else
+//    Console.WriteLine("Even Number");
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Item: {i}");
+}
+
